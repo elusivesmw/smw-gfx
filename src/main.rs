dcf8684a3633065f961c4a46eee6870162fb9c90
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-08-31 22:10:44 -0700

    print bin file to console

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,29 @@ fn main() {
 
     let path = "./in/GFX00.bin";
     let bin = fs::read(path).unwrap();
+    //print_first_bits(&bin);
 
-    let first_bits = bin[..16].iter()
-        .map(|b| format!("{:08b}", b).to_string())
-        .collect::<Vec<String>>()
-        .join(" ");
-    println!("{}", first_bits);
-    //println!("{:x?}", &contents[..16]);
-
-
-    //let data = 
-    bin_to_4bpp(bin);
-
+    let converted = bin_to_4bpp(&bin);
+    print_4bpp(&converted);
 }
 
-
 const SIZE: usize = 32; // 4bpp = 32 bytes per 8x8
-fn bin_to_4bpp(bin: Vec<u8>) -> () { // gfx_4bpp {
-    //let mut tiles: Vec<Vec<u8>> = Vec::new();
-    for (i, val) in bin.iter().enumerate().step_by(32) {
+fn bin_to_4bpp(bin: &Vec<u8>) -> Vec<Vec<u8>> {
+    let mut tiles: Vec<Vec<u8>> = Vec::new();
+    for (i, _val) in bin.iter().enumerate().step_by(SIZE) {
         let slice = &bin[i..i+SIZE];
-        let tile = slice_to_tile(slice);
-        print_tile(tile);
+        let tile = slice_to_tile(&slice);
 
-        //tiles.push(tile);
-        //if i == 64 { break; } // just one 8x8 row for now
+        //print_tile(&tile);
+        tiles.push(tile);
     }
-
+    tiles
 }
 
 fn slice_to_tile(slice: &[u8]) -> Vec<u8> {
-    //println!("{:?}", slice);
     let mut tile = Vec::new();
 
-    for r in 0..8 {//8 {
+    for r in 0..8 {
         let r = r * 2;
         //println!("{:?}", r);
         let bp1 = slice[r + 0];
@@ -48,26 +37,15 @@ fn slice_to_tile(slice: &[u8]) -> Vec<u8> {
 
         // translate to an array of colors for this row
         for c in (0..8).rev() {
-            // c represents each column in a row
-
             let palette = get_pixel_color(c, bp1, bp2, bp3, bp4);
-            //println!("pixel {:x?} = palette {}", c, palette);
-
-            // palette byte to 2 pixels (for bpp4)
-            let (px1, px2) = u8_to_u4s(c);
             tile.push(palette);
-            //tile.push(px1);
-            //tile.push(px2);
         }
     }
 
-    //println!("{:?}", slice);
-    //println!("{:?}", tile);
     tile
 }
 
 fn get_pixel_color (c: u8, bp1: u8, bp2: u8 , bp3: u8, bp4: u8) -> u8 {
-    let palette: u8 = 0;
     let mask = 1 << c;
 
     let px_bp1 = if (bp1 & mask) == mask { 1 } else { 0 };
@@ -81,7 +59,16 @@ fn get_pixel_color (c: u8, bp1: u8, bp2: u8 , bp3: u8, bp4: u8) -> u8 {
     color
 }
 
-fn print_tile(tile: Vec<u8>) {
+
+fn print_first_bits(bin: &Vec<u8>) {
+    let first_bits = bin[..16].iter()
+        .map(|b| format!("{:08b}", b).to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+    println!("{}", first_bits);
+}
+
+fn print_tile(tile: &Vec<u8>) {
     for (i, px) in tile.iter().enumerate() {
         if i % 8 == 0 { println!(); }
         print!("{:x?}", px);
@@ -89,16 +76,28 @@ fn print_tile(tile: Vec<u8>) {
     println!();
 }
 
-fn u8_to_u4s (byte: u8) -> (u8, u8) {
-    let msbs = (byte & 0xf0) >> 4;
-    let lsbs = byte & 0x0f;
-    //println!("in {} = {},{}", byte, msbs, lsbs);
-    (msbs, lsbs)
+
+fn print_4bpp(converted: &Vec<Vec<u8>>) {
+    let num_rows = converted.iter().len() / 16; // tiles per row
+    for tri in 0..num_rows {
+        let tr = &converted[tri*16..tri*16 + 16]; // tiles per row
+        for pri in 0..8 {
+            for ti in 0..16 {
+                let tile = &tr[ti];
+                for pi in 0..8 {
+                    let p = &tile[pri * 8 + pi];
+                    print!("{}", p);
+                }
+                print!(" ");
+            }
+            println!();
+        }
+    }
 }
 
 /*
-fn u8_to_u2s (byte: u8) -> (u8, u8, u8, u8) {
-    let msbs = (byte & 0x11000000) >> 6;
+fn u8_to_u4s (byte: u8) -> (u8, u8) {
+    let msbs = (byte & 0xf0) >> 4;
     let lsbs = byte & 0x0f;
     //println!("in {} = {},{}", byte, msbs, lsbs);
     (msbs, lsbs)
