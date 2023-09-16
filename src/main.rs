ca52474fe96fa106c9cb5dc4170691ecda177626
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-09-15 20:43:32 -0700

    add support for 1-4bpp formats

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,18 @@ fn main() {
     let bin = fs::read(path).unwrap();
     //print_first_bits(&bin);
 
-    let converted = bin_to_4bpp(&bin);
+    let converted = bin_to_tiles(&bin, Bpp::_4bpp);
     print_4bpp(&converted);
 }
 
-const SIZE: usize = 32; // 4bpp = 32 bytes per 8x8
-fn bin_to_4bpp(bin: &Vec<u8>) -> Vec<Tile> {
+fn bin_to_tiles(bin: &Vec<u8>, format: Bpp) -> Vec<Tile> {
+    let bytes_per_8x8 = format.bytes_per_8x8();
+    //println!("{}", bytes_per_8x8);
     let mut tiles: Vec<Tile> = Vec::new();
-    for (i, _val) in bin.iter().enumerate().step_by(SIZE) {
-        let slice = &bin[i..i+SIZE];
-        let tile = slice_to_tile(&slice);
+    for (i, _val) in bin.iter().enumerate().step_by(bytes_per_8x8) {
+        if i+bytes_per_8x8 > bin.iter().count() { println!("warn: unexpected file length"); break; }
+        let slice = &bin[i..i+bytes_per_8x8];
+        let tile = slice_to_tile(&slice, format);
 
         //print_tile(&tile);
         tiles.push(tile);
@@ -26,42 +28,88 @@ fn bin_to_4bpp(bin: &Vec<u8>) -> Vec<Tile> {
     tiles
 }
 
+
+#[derive(Copy,Clone)]
+enum Bpp {
+    _1bpp = 1,
+    _2bpp = 2,
+    _3bpp = 3,
+    _4bpp = 4,
+}
+
+impl Bpp {
+    fn val (&self) -> u8 {
+        match self {
+            Bpp::_1bpp => { Bpp::_1bpp as u8 }
+            Bpp::_2bpp => { Bpp::_2bpp as u8 }
+            Bpp::_3bpp => { Bpp::_3bpp as u8 }
+            Bpp::_4bpp => { Bpp::_4bpp as u8 }
+        }
+    }
+
+    fn bytes_per_8x8(&self) -> usize {
+        let val = self.val() as usize;
+        val * 8 as usize
+    }
+}
+
 struct Tile {
-    //format: 
-    pixels: Vec<u8>,
+    bpp: Bpp,
+    data: Vec<u8>,
 }
 
 impl Tile {
-    fn new() -> Self {
+    fn new(bpp: Bpp) -> Self {
         Self {
-            pixels: Vec::new()
+            bpp,
+            data: Vec::new(),
         }
     }
 
     fn get(&self, x: usize, y: usize) -> u8 {
-        self.pixels[y * 8 + x]
+        self.data[y * 8 + x]
     }
 
     fn set(&mut self, x: usize, y: usize, val: u8) {
-        self.pixels[y * 8 + x] = val;
+        self.data[y * 8 + x] = val;
     }
 }
 
-fn slice_to_tile(slice: &[u8]) -> Tile {
-    let mut tile = Tile::new();
+fn slice_to_tile(slice: &[u8], bpp: Bpp) -> Tile {
+    let mut tile = Tile::new(bpp);
+    let mut bp1 = 0;
+    let mut bp2 = 0;
+    let mut bp3 = 0;
+    let mut bp4 = 0;
 
     for r in 0..8 {
-        let r = r * 2;
-        //println!("{:?}", r);
-        let bp1 = slice[r + 0];
-        let bp2 = slice[r + 1];
-        let bp3 = slice[r + 16];
-        let bp4 = slice[r + 17];
-
+        match bpp {
+            Bpp::_1bpp => {
+                bp1 = slice[r];
+            }
+            Bpp::_2bpp => {
+                let r = r * 2;
+                bp1 = slice[r + 0];
+                bp2 = slice[r + 1];
+            }
+            Bpp::_3bpp => {
+                bp1 = slice[r*2 + 0];
+                bp2 = slice[r*2 + 1];
+                bp3 = slice[16 + r];
+            }
+            Bpp::_4bpp => {
+                let r = r * 2;
+                //println!("{:?}", r);
+                bp1 = slice[r + 0];
+                bp2 = slice[r + 1];
+                bp3 = slice[r + 16];
+                bp4 = slice[r + 17];
+            }
+        }
         // translate to an array of colors for this row
         for c in (0..8).rev() {
             let palette = get_pixel_color(c, bp1, bp2, bp3, bp4);
-            tile.pixels.push(palette);
+            tile.data.push(palette);
         }
     }
 
